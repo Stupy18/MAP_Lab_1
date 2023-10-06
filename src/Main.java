faf032b274e2f558df525afa3323c5936f968b03
Stupy18 <stupariubogdan@gmail.com>
2023-10-06 13:10:02 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        NotenProzessor notenProzessor = new NotenProzessor();
+
+        int[] noten = new int[] { 10, 54, 30, 39, 47, 48, 60 };
+
+        System.out.println(Arrays.toString(notenProzessor.nichtAusreichendeNoten(noten)));
+    }
+}
